6a451bc63aa33a9b9f667881d608cc74288398d3
Darshan Patil <iampatildarsh>
2022-12-19 23:29:15 +0530

    Ability to create a Contacts in Address Book

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,5 +1,8 @@
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        Contacts contacts = new Contacts("Darshan","Patil","Karve Nagar","Pune", "Maharashtra",411048,8379050224L,"iampatildarsh@gmail.com");
+        System.out.println(contacts);
     }
 }
